d386e178374b8e60e5311fe594c3972564017dee
Ryan Rauh <rauh.ryan@gmail.com>
2026-05-09 09:29:30 -0400

    🐛 add memmove shim for wasm builds

--- src/mem.c
+++ src/mem.c
@@ -10,6 +10,27 @@ void *memcpy(void *dst, const void *src, size_t n) {
   return dst;
 }
 
+void *memmove(void *dst, const void *src, size_t n) {
+  unsigned char *d = (unsigned char *)dst;
+  const unsigned char *s = (const unsigned char *)src;
+
+  if (d == s || n == 0) {
+    return dst;
+  }
+
+  if (d < s) {
+    while (n--)
+      *d++ = *s++;
+  } else {
+    d += n;
+    s += n;
+    while (n--)
+      *--d = *--s;
+  }
+
+  return dst;
+}
+
 void *memset(void *dst, int c, size_t n) {
   unsigned char *d = (unsigned char *)dst;
   while (n--)
